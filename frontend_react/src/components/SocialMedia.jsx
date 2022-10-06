a09e3b3226d909a6563ee3a0d9048480f8ee83d7
Shashank Agrawal <agrawal.sha@northeastern.edu>
2022-10-05 23:41:45 -0400

    changed social media icon colors

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -4,17 +4,17 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
+    <div className="app__social__github">
       <a href='https://github.com/sagrawal8' target="_blank" rel="noopener noreferrer">
         <FaGithub />
       </a>
     </div>
-    <div>
+    <div className="app__social__linkedin">
       <a href='https://www.linkedin.com/in/agrawal-shashank/' target="_blank" rel="noopener noreferrer">
         <FaLinkedin />
       </a>
     </div>
-    <div>
+    <div className="app__social__insta">
       <a href='https://www.instagram.com/dfaltd/' target="_blank" rel="noopener noreferrer">
         <BsInstagram />
       </a>
--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 
 import App from './App';
 import './index.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
